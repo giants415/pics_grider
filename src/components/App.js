2213b1edae52c9c214ef804c34a5552bd24360ef
Andrew_Vinocur <andrew.vinocur@gmail.com>
2019-01-03 16:47:18 -0800

    refactors to remove error and update state with axios response results

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar'
 class App extends React.Component {
   state = { images: [] };
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
     const res = await axios
       .get('https://api.unsplash.com/search/photos', {
         params: { query: term },
@@ -15,13 +15,14 @@ class App extends React.Component {
         }
       });
 
-    console.log(res.data.results);
+    this.setState({ images: res.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
