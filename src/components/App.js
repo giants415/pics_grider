8e5bdd32a097da5106b750ffcc9b1d8ca71f8b98
Andrew_Vinocur <andrew.vinocur@gmail.com>
2019-01-03 16:29:46 -0800

    inits state as empty array

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
+  state = { images: [] };
+
   async onSearchSubmit(term) {
     const res = await axios
       .get('https://api.unsplash.com/search/photos', {
