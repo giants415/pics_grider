d396b856b0e76ccf66bd93806ea846605ea4a46d
Andrew_Vinocur <andrew.vinocur@gmail.com>
2019-01-08 16:54:24 -0800

    improves code via destructuring

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img key={image.id} alt={image.description} src={image.urls.regular} />
+    // destructures out image and only uses data we need
+  const images = props.images.map(({ description, id, urls }) => {
+
+    return <img key={id} alt={description} src={urls.regular} />
   });
 
   return <div>{images}</div>;
