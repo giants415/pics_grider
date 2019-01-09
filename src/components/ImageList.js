64a31f2950ac5bb8a9100c0e13c7b5912ae81233
Andrew_Vinocur <andrew.vinocur@gmail.com>
2019-01-09 10:52:25 -0800

    removes old note

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,7 +4,6 @@ import React from 'react';
 import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-    // destructures out image and only uses data we need
   const images = props.images.map(image => {
     return <ImageCard key={image.id} image={image}/>
   });
